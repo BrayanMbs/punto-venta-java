98301c22b834a439a3d37769e0590b1637881d2f
BrayanMbs <brayanmatias.2020@gmail.com>
2025-06-05 02:02:50 -0600

    se implementaron todas las configuraciones de la base de datos

---
+++ backend/src/main/java/com/example/punto_venta/PuntoVentaApplication.java
@@ -0,0 +1,13 @@
+package com.example.punto_venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntoVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntoVentaApplication.class, args);
+		System.out.println("\u001B[32m" + "\n(NO ERROR), EL PROGRAMA ESTA CORRIENDO!" + "\u001B[0m");
+	}
+}
